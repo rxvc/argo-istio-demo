ebb73c669d70b8d5bbfc29b1bf72887bb04e87a5
Rodrigo Vallejo <rxvallejoc@gmail.com>
2025-05-16 16:57:51 -0500

    chore: improve istio installation

--- install-istio-sidecard.sh
+++ install-istio-sidecard.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 echo "Install istio ..."
+echo "Install istio base ..."
 kubectl apply -f istio/sidecard/istio-base-application.yaml -n argocd
+echo "Install istiod ..."
 kubectl apply -f istio/sidecard/istiod-application.yaml -n argocd 
+sleep 20
+echo "Install metalib for kind loadbalancer"
+bash kind/loadBalancer/install-metalib.sh
+echo "Install istio ingressgateway ..."
 kubectl apply -f istio/sidecard/istio-ingressgateway-application.yaml -n argocd  
--- kind/loadBalancer/install-metalib.sh
+++ kind/loadBalancer/install-metalib.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sleep 10
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.12/config/manifests/metallb-native.yaml
+sleep 20
 kubectl apply -f kind/loadBalancer/metallb-config.yaml

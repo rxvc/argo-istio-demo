6c64bf56ecd789bb35631fc65d537b2d4178b033
Rodrigo Vallejo <rxvallejoc@gmail.com>
2025-05-15 22:57:26 -0500

    feat: add istio proxyv2

--- install-istio.sh
+++ install-istio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Install istio ..."
-kubectl apply -f istio/istio-ingress-gateway-application.yaml -n argocd
+kubectl apply -f istio/istio-base-application.yaml -n argocd
 kubectl apply -f istio/istiod-application.yaml -n argocd 
 kubectl apply -f istio/istio-ingress-gateway-application.yaml -n argocd  
